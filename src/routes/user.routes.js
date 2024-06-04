e49d9ad179c03c8ea48f4425a622c7f3b3e47337
dhruv0403 <dhruvagrawal2204@gmail.com>
2024-06-05 04:32:09 +0530

    add user controller , routes and auth middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router} from "express";
-import { registerUser } from "../controllers/user.controllers.js";
+import { loginuser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router()
 
 
@@ -16,5 +17,12 @@ router.route("/register").post(
         }
     ]),
     registerUser)
+
+router.route("/login").post(loginuser);
+
+//secured routes
+
+router.route("/logout").post(verifyJWT,logoutUser);
+router.route("refresh-token").post(refreshAccessToken);
 
 export default router
